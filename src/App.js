5191d2fb7732f07a5f363bfcbaba0a208e9ad088
Konstantin <whitehorse.spb@gmail.com>
2021-08-23 21:47:49 +0300

    created Registr

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { SignIn } from "./Registr";
+// // import { Step2 } from "./Step2";
+// // import { Step3 } from "./Step3";
+// // import { Result } from "./Result";
+// import { Header } from "./Header";
+
+
+
+const About = () => <h1>Главная страница</h1>
+// // const Step1 =() => <>Step1</>
+// const Step2 =() => <>Step2</>
+// const Step3 =() => <>Step3</>
+// const Result =() => <>Result</>
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Router> 
+        <Switch>
+          <Route exact path="/" component={SignIn} />  
+          <Route path="/about" component={About} />  
+        </Switch>
+      </Router>
+    </>
   );
 }
 
