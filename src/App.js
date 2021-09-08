43f4f7a0adaf2b46a328f7cfaaa05abb1c8b30f9
Konstantin <whitehorse.spb@gmail.com>
2021-09-08 23:40:19 +0300

    add_glavnaya

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import { Login } from "./Registr";
-// // import { Step2 } from "./Step2";
-// // import { Step3 } from "./Step3";
-// // import { Result } from "./Result";
-// import { Header } from "./Header";
+import { Login } from "./pages/Registr";
+
 
 
 
 const About = () => <h1>Главная страница</h1>
-// // const Step1 =() => <>Step1</>
-// const Step2 =() => <>Step2</>
-// const Step3 =() => <>Step3</>
-// const Result =() => <>Result</>
 
 
 
