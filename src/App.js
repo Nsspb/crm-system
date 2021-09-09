3597092777e338af8817c05805623c8d2f058efd
Konstantin <whitehorse.spb@gmail.com>
2021-09-09 18:01:33 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,9 +1,3 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import { Login } from "./pages/Registr";
-
-
-
 
 const About = () => <h1>Главная страница</h1>
 
@@ -14,12 +8,7 @@ const About = () => <h1>Главная страница</h1>
 function App() {
   return (
     <>
-        <Router> 
-        <Switch>
-          <Route exact path="/" component={Login} />  
-          <Route path="/about" component={About} />  
-        </Switch>
-      </Router>
+     Hello kitty
     </>
   );
 }
