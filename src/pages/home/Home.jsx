1506a2981c1c9e4b3156ea3e68d9fe9e293dc5d8
Konstantin <whitehorse.spb@gmail.com>
2021-09-11 02:48:38 +0300

    create new element

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../../components/header/Header";
+import Leftbar from "../../components/leftbar/Leftbar";
+import Rightbar from "../../components/rightbar/Rightbar";
+
+
+export default function Home() {
+    return (
+        <>
+           <Header />
+           <Rightbar />
+           <Leftbar />
+        </>
+    )
+}
