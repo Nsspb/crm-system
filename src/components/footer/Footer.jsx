f047cd2223ad4fed451bc2398878301c3ac76b9d
Konstantin <whitehorse.spb@gmail.com>
2021-09-12 23:28:38 +0300

    add_post

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,13 @@
 import './footer.css'
 import Share  from '../share/Share'
+import Post from '../post/Post'
 
 export default function Footer() {
     return (
         <div className='footer' >
            <div className="footerWrapper">
                <Share />
+               <Post />
            </div>
         </div>
     )
--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,14 +1,43 @@
 
 import './share.css'
+import {PermMedia, Room, EmojiEmotions, Label} from '@mui/icons-material'
+import logo from './person/autor.jpeg'
 
 export default function Share() {
     return (
         <div className='share'>
             <div className="shareWrapper">
+               
                 <div className="shareTop">
-                    <img src="" alt="" className="shareProfileImg" />
+                    <img src={logo} alt="" className="shareProfileImg" />
+                    <input placeholder='О чем сейчас думаешь?' className="shareInput" />
                 </div>
-                <div className="shareBottom"></div>
+               
+                <hr className='shareHr' />    
+               
+                <div className="shareBottom">
+                    <div className="shareOptions">
+                     
+                        <div className="shareOption">
+                            <PermMedia htmlColor='red' className='shareIcon' />
+                            <span className='shareOptionText1' > Фото/видео</span>
+                        </div>
+                       
+                        <div className="shareOption">
+                            <Label  htmlColor='blue' className='shareIcon' />
+                            <span className='shareOptionText' > Заметки</span>
+                        </div>
+                        <div className="shareOption">
+                            <Room htmlColor='green' className='shareIcon' />
+                            <span className='shareOptionText' > Локация</span>
+                        </div>
+                        <div className="shareOption">
+                            <EmojiEmotions htmlColor='rgb(213, 217, 4)' className='shareIcon' />
+                            <span className='shareOptionText' > "Эмоции" </span>
+                        </div>
+                    </div>
+                <button className='shareButton'>Поделиться</button>
+                </div>
             </div>
         </div>
     )
