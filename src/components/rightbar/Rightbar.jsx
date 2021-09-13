b0f08e18d20211bd317b78b56b9b6cb333c65d42
Konstantin <whitehorse.spb@gmail.com>
2021-09-13 04:21:16 +0300

    change_rightbar_friends

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,9 +1,29 @@
 import './rightbar.css'
+import present from './images/birthday.png'
+import reclama from './images/reclama.gif'
+import img2 from './images/2.jpeg'
 
 export default function Rightbar() {
     return (
         <div className='rightbar' >
-            rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img src={present} alt="" className="birthdayImg" />
+                    <span className="birthdayText">
+                        <b>Екатерина</b> и <b> друзья </b> празднуют день рождения   
+                    </span>
+                </div>
+                <img src={reclama} alt="" className="righthbarReclama" />
+                <h4 className="rightbarTitle">Друзья онлайн</h4>
+                <ul className="rightbarFriendsList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src={img2} alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
