cdefa72f588c930698b4361b14d4372dbbfa13f5
Konstantin <76496638+Nsspb@users.noreply.github.com>
2021-10-06 01:35:37 +0300

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,19 +1,41 @@
 import './footer.css'
+
 import Share  from '../share/Share'
 import Post from '../post/Post'
-import {Posts} from "../../dummyData" 
+import {useContext, useEffect, useState  } from 'react';
+import { AuthContext } from "../../context/AuthContext";
+
+import axios from "axios"
 
 
-export default function Footer() {
+
+export default function Footer({ username }) {
+    const [posts, setPosts] = useState([]);
+    const { user } = useContext(AuthContext);
+  
+    useEffect(() => {
+      const fetchPosts = async () => {
+        const res = username
+          ? await axios.get("/posts/profile/" + username)
+          : await axios.get("posts/timeline/" + user._id);
+        setPosts(
+          res.data.sort((p1, p2) => {
+            return new Date(p2.createdAt) - new Date(p1.createdAt);
+          })
+        );
+      };
+      fetchPosts();
+    }, [username, user._id]);
+  
     return (
-        <div className='footer' >
-           <div className="footerWrapper">
-               <Share />
-               {Posts.map((p) =>(
-                   <Post key={p.id} post={p}/>
-                   ))}
-    
-           </div>
+      <div className="footer">
+        <div className="footerWrapper">
+          {(!username || username === user.username) && <Share />}
+          {posts.map((p) => (
+            <Post key={p._id} post={p} />
+          ))}
         </div>
-    )
-}
+      </div>
+    );
+  }
+  
