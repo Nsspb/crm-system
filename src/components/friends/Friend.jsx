46faf97d54613e7d566235c416915184952aa64a
Konstantin <76496638+Nsspb@users.noreply.github.com>
2021-10-06 01:37:08 +0300

    Update Friend.jsx

--- src/components/friends/Friend.jsx
+++ src/components/friends/Friend.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+
 import "./friend.css"
 
 
-export default function Friend(user) {
+export default function Friend({user}) {
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
     return (
         <li className="leftbarFriend">
-        <img src="" alt="" className="leftbarFriendsImg" />
+        <img   className="leftbarFriendsImg"  src={PF + user.profilePicture} alt=""/>
         <span className="leftbarFriendName">{user.username}</span>
     </li>
     )
