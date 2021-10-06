69286dcebe40e3f63aebb23b63b92f85ec54d3cf
Konstantin <whitehorse.spb@gmail.com>
2021-10-06 04:01:52 +0300

    small_change

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -73,7 +73,7 @@ export default function Post({ post }) {
                         />
                         <img
                             className='likeIcon'
-                            src={`${PF}heart.png`}
+                            src={`${PF}dislike.png`}
                             onClick={likeHandler}
                             alt=''
                         />
